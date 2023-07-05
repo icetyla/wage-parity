3eb07e6a70c62ce74f750f9a19dcc332518e1644
icetyla <isaaccjay@gmail.com>
2023-07-04 20:10:27 -0400

    Balance the dataset

--- regression.py
+++ regression.py
@@ -4,23 +4,36 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
+from imblearn.over_sampling import SMOTE
 
 # Load the data
 path = os.getcwd()
 train = pd.read_csv(path + '/train.csv')
 test = pd.read_csv(path + '/test.csv')
 
+# Build a label
 train['overspend'] = (train.weekly_spending > train.allocated_wage).astype(int)
 
-# Create variables for the model
+# Check for data imbalances
+class_one = len(train[train.overspend == 0])
+class_two = len(train[train.overspend == 1])
+
+print('0: %d, 1: %d'%(class_one, class_two))
+
+# Split dataset into variables and balance the label
 X = train.drop(['index', 'first_name', 'last_name', 'agency', 'overspend'], axis=1)
 X_test = test.drop(['index', 'first_name', 'last_name', 'agency'], axis=1)
 y = train.overspend
+sm = SMOTE()
+X_sm, y_sm = sm.fit_resample(X, y)
 
-# Split the training data
-X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, train_size=0.8, random_state=0)
+print(y_sm.value_counts())
 
-# Fit the model with the training data
+
+X_train, X_valid, y_train, y_valid = train_test_split(X_sm, y_sm, test_size=0.2, train_size=0.8, random_state=0)
+
+
+# Fit the model
 logistic = LogisticRegression()
 logistic.fit(X_train, y_train)
 preds = logistic.predict(X_valid)
